65d772289838652dabace0eff635e87b8f096508
Renal Apriansyah <renal.apr@gmail.com>
2022-07-19 19:01:34 +0700

    sesi 2 - array method & function

--- getCode.js
+++ getCode.js
@@ -1,16 +1,46 @@
 const data = require('./peserta.json')
 
-// names = ['lukman hakim', 'WS', 'Dony Dony', 'aristya kusmanugraha']
 
-const studentNames = $$('div[role="listitem"]')
-	.map((v, i) => v.children[0].querySelector('span').textContent)
-	.filter(name => name!='Renal Apriansyah')
-    
-const presents = studentNames.map(name => {
-    if(data[name]) {
-        return data[name].code.match(/(?<=RCTN064ONL).*/)[0].replace(/0+/, '')
-    }
-    return null
+
+// const studentNames = $$('div[role="listitem"]')
+// 	.map((v, i) => v.children[0].querySelector('span').textContent)
+// 	.filter(name => name!='Renal Apriansyah')
+studentNames = [
+    "adam wahidin",
+    "adi guna",
+    "Ahmad Romadhon",
+    "Ali Suswanto",
+    "alif ramadhan",
+    "aristya kusmanugraha",
+    "Dony Dony",
+    "Fadillah Maulana",
+    "Farid Hakim",
+    "Farid Rahmat",
+    "Felix Hendrian",
+    "Geoffrey Laurens",
+    "kenny adam",
+    "Louis Gunawan S",
+    "lukman hakim",
+    "Lutfi Dwimulya",
+    "M. Arief Hidayat",
+    "Muhammad Sidiq Satyaji",
+    "Rio Kiswandaru",
+    "Septian",
+    "Sonita Veronica Barus",
+    "tybachri",
+    "ucupcup 21",
+    "vicky fernandi",
+    "wahyu komara",
+    "Wiwit Setiyo P",
+    "Yasin Mochamad",
+    "Yulius Susilo"
+]
+// studentNames = ['adam wahidin', 'adi guna', 'Ahmad Romadhon', 'Ali Suswanto', 'alif ramadhan', 'aristya kusmanugraha', 'Bayu Rmk', 'Dony Dony', 'Fadillah Maulana', 'Farid Hakim', 'Farid Rahmat', 'Felix Hendrian', 'Geoffrey Laurens', 'kenny adam', 'Louis Gunawan S', 'Lutfi Dwimulya', 'M. Arief Hidayat', 'Muhammad Sidiq Satyaji', 'Rio Kiswandaru', 'Septian', 'Sonita Veronica Barus', 'tybachri', 'ucupcup 21', 'vicky fernandi', 'wahyu komara', 'Wiwit Setiyo P', 'Yasin Mochamad', 'Yulius Susilo']
+const presents = studentNames
+.filter(name => !!data[name])
+.map(name => {
+    return data[name].code.match(/(?<=RCTN064ONL).*/)[0].replace(/0+/, '')
 })
 const presentForm = presents.sort((a,b) => a-b).join(',')
 console.log(presentForm, `<===================  ==================`);
+// 1,2,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30
--- sesi-2.js
+++ sesi-2.js
@@ -56,8 +56,8 @@ add([2,3], sum)
 // showMessage('hello')
 // console.log(sumImplicitReturn(1,2), `<===================  ==================`);
 
-console.log(typeof [], `<===================  ==================`);
-console.log(typeof {},`<===================  ==================`);
+// console.log(typeof [], `<===================  ==================`);
+// console.log(typeof {},`<===================  ==================`);
 
 let person = {
     name: 'udin',
@@ -67,7 +67,7 @@ let person = {
 
 const { name: udinName, height } = person
 
-console.log(udinName, height, `<===================  ==================`);
+// console.log(udinName, height, `<===================  ==================`);
 
 let cars = ['audi', 'bmw', 'cherry']
 
@@ -82,4 +82,94 @@ const getPerson = () => {
 
 const {name: bambangName, lastName} = getPerson()
 
-console.log(bambangName, lastName, `<===================  ==================`);
+// console.log(bambangName, lastName, `<===================  ==================`);
+
+
+
+let counter = {
+    num: 0,
+    add: function() {
+        console.log(this, '<======this======')
+    }
+}
+
+this.test = 'test'
+function normalFunction() {
+    console.log(this, `<=================== norm  ==================`);
+}
+
+const arrow = () => {
+    console.log(this, `<=================== arrow ==================`);
+}
+
+function Counter() {
+    this.val = 0
+    
+    setTimeout(() => {
+        console.log(this, `<=================== arrow  ==================`);
+    }, 1500)
+}
+
+function Counter2() {
+    this.val = 0
+    
+    setTimeout(function() {
+        console.log(this, `<=================== non arrow  ==================`);
+    }, 1500)
+}
+
+// Counter2()
+// Counter()
+
+
+const tambah = (num1, num2) => num1 + num2
+
+const jumlah = (arr) => {
+    const num1 = arr[0] * 2
+    const num2 = arr[1] * 3
+    
+    return (param3) => {
+        return num1 + num2 + param3
+    }
+}
+
+const res = jumlah([2,1])(4)
+
+const animals = [
+    {name: 'fluffy', species: 'cat'},
+    {name: 'carlo', species: 'cat'},
+    {name: 'sweety', species: 'fish'},
+    {name: 'bruno', species: 'dog'},
+]
+
+let getAnimalNames = (animals) => {
+    let animalNames = []
+    for (let i=0;i<animals.length;i++) {
+        animalNames.push(animals[i].name)
+    }
+    return animalNames
+}
+
+let getAnimalNamesMap = animals => animals
+    .filter(animal => animal.species === 'cat')
+    .map(animal => animal.name)
+    
+let orders = [
+    {total: 100},
+    {total: 200},
+    {total: 300}
+]
+
+let getTotal = (arr) => {
+    let total = 0
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i].total
+    }
+    
+    return total
+}
+
+let getTotalReduce = (arr) => 
+    arr.reduce((acc, order) => acc + order.total, 0)
+
+console.log(getTotalReduce(orders), `<===================  ==================`);
