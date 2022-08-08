aedf8b0f530ae000c7edf40b258c6c1c14bd02a8
Renal Apriansyah <renal.apr@gmail.com>
2022-08-08 19:51:24 +0700

    absen sesi 9

--- getCode.js
+++ getCode.js
@@ -9,20 +9,21 @@ names = [
     "adam wahidin",
     "adi guna",
     "Ahmad Romadhon",
+    "Ali Suswanto",
     "alif ramadhan",
+    "aristya kusmanugraha",
     "Dony Dony",
     "Fadillah Maulana",
+    "Farid Hakim",
     "Farid Rahmat",
     "Felix Hendrian",
     "Geoffrey Laurens",
     "kenny adam",
     "Louis Gunawan S",
-    "Louis Gunawan S",
     "M. Arief Hidayat",
     "Muhammad Sidiq Satyaji",
     "Rio Kiswandaru",
     "Septian",
-    "tybachri",
     "vicky fernandi",
     "wahyu komara",
     "Yasin Mochamad",
@@ -34,4 +35,4 @@ const presents = names
     .map(name => data[name].code.match(/(?<=RCTN064ONL).*/)[0].replace(/0+/, ''))
 const presentForm = presents.sort((a,b) => a-b).join(',')
 console.log(presentForm, `<===================  ==================`);
-// 2,5,6,8,10,11,13,14,15,18,19,20,21,22,23,24,25,26,27,29,30
+// 2,5,6,7,8,10,11,13,14,15,18,19,20,21,22,23,24,25,26,27,28,29
