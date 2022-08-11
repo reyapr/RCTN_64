ba1bd1ddaa85e41b02a3f4e0c8055b2c0019e07b
Renal Apriansyah <renal.apr@gmail.com>
2022-08-11 17:56:12 +0700

    sesi 12 - ssr

---
+++ sesi-12/my-app/pages/index.jsx
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  const [users, setUsers] = useState([])
+  
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => {
+        setUsers(data)
+      })
+  }, [])
+  
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {
+          users.map(user => {
+            return (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
+  )
+}
