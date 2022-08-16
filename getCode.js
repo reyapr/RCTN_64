776fc45f363b5e406cfdcf6be2d287120af5c6cb
Renal Apriansyah <renal.apr@gmail.com>
2022-08-16 18:27:55 +0700

    sesi 13 - absen

--- getCode.js
+++ getCode.js
@@ -7,23 +7,21 @@ const data = require('./peserta.json')
 // 	.filter(name => name!='Renal Apriansyah')
 names = [
     "adam wahidin",
-    "adi guna",
     "Ahmad Romadhon",
-    "alif ramadhan",
+    "Ali Suswanto",
     "aristya kusmanugraha",
+    "Dony Dony",
     "Fadillah Maulana",
     "Farid Hakim",
-    "Farid Rahmat",
-    "Felix Hendrian",
     "Geoffrey Laurens",
     "kenny adam",
+    "Louis Gunawan S",
     "M. Arief Hidayat",
     "Muhammad Sidiq Satyaji",
     "Rio Kiswandaru",
     "Septian",
     "vicky fernandi",
-    "WS",
-    "Yulius Susilo",
+    "Yasin Mochamad",
     "Yusup Supriyadi"
 ]
 const presents = names
@@ -31,4 +29,5 @@ const presents = names
     .map(name => data[name].code.match(/(?<=RCTN064ONL).*/)[0].replace(/0+/, ''))
 const presentForm = presents.sort((a,b) => a-b).join(',')
 console.log(presentForm, `<===================  ==================`);
-// 1,7,8,9,10,11,12,13,14,15,18,19,20,21,22,23,24,25,26,27,29,30
+// 2,5,6,7,9,10,13,18,19,20,21,22,24,25,26,28,29
+
